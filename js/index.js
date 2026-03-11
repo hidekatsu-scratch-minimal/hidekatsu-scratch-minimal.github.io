0dfdbef5d65e1032984fec6f32e4fff64d7094dd
hidekatsu-scratch-minimal <hidekatsu.scratch@gmail.com>
2026-03-11 11:36:35 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
-document.getElementById("nyuuryoku").innerHTML = `
-<p>ドメイン<p><select id="domein">
-<option value="new">ドメインを選択</option>
-<option value="atten">atten.win</option>
-<option value="scratch">scratch.mit.edu</option>
-</select>
-<div class="konoato"></div>`;
+// 1. HTMLを挿入（HTML側の <div class="nyuuryoku"> も id="nyuuryoku" に直してください）
+const container = document.getElementById("nyuuryoku");
 
-const select = document.getElementById('nyuuryoku');
-select.addEventListener('change' , (e) => {
-  const selectedValue = e.target.value;
-  
-  if (selectedValue === 'atten') {
-    console.log('atten');
-  } else if (selectedValue === 'scratch') {
-    console.log('scratch');
-  }
-});
+if (container) {
+  container.innerHTML = `
+    <p>ドメイン</p>
+    <select id="domein">
+      <option value="new">ドメインを選択</option>
+      <option value="atten">atten.win</option>
+      <option value="scratch">scratch.mit.edu</option>
+    </select>
+    <div class="konoato"></div>`;
+
+  // 2. 挿入した select 要素に対してイベントを設定
+  const selectElement = document.getElementById('domein');
+  selectElement.addEventListener('change', (e) => { // ←カンマを追加！
+    const selectedValue = e.target.value;
+    console.log("選択された値:", selectedValue);
+
+    if (selectedValue === 'atten') {
+      console.log('attenを選択しました');
+    } else if (selectedValue === 'scratch') {
+      console.log('scratchを選択しました');
+    }
+  });
+}
